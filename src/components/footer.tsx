d89fded2d8c04a26678d1bbfcbdf788e3f2ea0cb
Guoqi Sun <sun412926@gmail.com>
2024-10-05 22:41:17 +0800

    fix: fix style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,25 @@
 export default function Footer() {
   return (
-    <div className="my-10 flex">
-      <p>Designed By</p>
-      <a href="https://blog.sunguoqi.com" className="mx-1 hover:text-blue-500">
-        Guoqi Sun
-      </a>
-      <p className="mx-1">|</p>
-      <p> Powered By </p>
-      <a
-        href="https://github.com/sun0225SUN/astro-air"
-        className="mx-1 hover:text-blue-500"
-      >
-        Astro Air
-      </a>
+    <div className="my-10 flex flex-col md:flex-row">
+      <div className="flex">
+        <p>Designed By</p>
+        <a
+          href="https://blog.sunguoqi.com"
+          className="mx-1 hover:text-blue-500"
+        >
+          Guoqi Sun
+        </a>
+      </div>
+      <div className="flex">
+        <p className="mx-1 hidden md:block">|</p>
+        <p> Powered By </p>
+        <a
+          href="https://github.com/sun0225SUN/astro-air"
+          className="mx-1 hover:text-blue-500"
+        >
+          Astro Air
+        </a>
+      </div>
     </div>
   )
 }
