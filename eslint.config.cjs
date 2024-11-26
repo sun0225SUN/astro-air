51e76a3b99586115f5fcd5c0fd87dcf2475f624f
Guoqi Sun <sun412926@gmail.com>
2024-11-26 20:20:21 +0800

    feat: add husky and lint staged

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -7,35 +7,6 @@ module.exports = [
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
-      // JavaScript rules
-      "prefer-const": "error",
-      // TypeScript rules
-      "@typescript-eslint/array-type": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/consistent-type-imports": [
-        "warn",
-        {
-          prefer: "type-imports",
-          fixStyle: "inline-type-imports",
-        },
-      ],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-        },
-      ],
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        {
-          checksVoidReturn: {
-            attributes: false,
-          },
-        },
-      ],
-      // React rules
-      "react/self-closing-comp": ["error"],
     },
   },
 ]
