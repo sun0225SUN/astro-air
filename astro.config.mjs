e96931ca477ba245f7a56fcf15f66136416034c5
Guoqi Sun <sun412926@gmail.com>
2024-11-27 03:52:52 +0800

    feat: add netlify ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from "astro/config"
-
+import netlify from "@astrojs/netlify"
 import react from "@astrojs/react"
-
 import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  adapter: process.env.NODE_ENV === "production" ? netlify() : undefined,
 })
