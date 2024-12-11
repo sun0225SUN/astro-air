821d87c568c9bf28cb3ed017f1611daef05dc680
Guoqi Sun <sun412926@gmail.com>
2024-12-11 21:25:58 +0800

    feat: add noise background image

---
+++ src/components/react/noise-bg.tsx
@@ -0,0 +1,19 @@
+export function NoiseBg() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        inset: 0,
+        zIndex: -1,
+        width: "100%",
+        height: "100%",
+        backgroundSize: "128px",
+        backgroundRepeat: "repeat",
+        backgroundImage:
+          "url('https://framerusercontent.com/images/rR6HYXBrMmX4cRpXfXUOvpvpB0.png')",
+        opacity: 0.06,
+        borderRadius: 0,
+      }}
+    ></div>
+  )
+}
--- src/components/react/test.tsx
+++
@@ -1,5 +0,0 @@
-export function Test() {
-  return (
-    <div className="text-3xl text-blue-500">Test React and TailwindCSS</div>
-  )
-}
