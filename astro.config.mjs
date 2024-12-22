a2c473709f46415f4d9909d92a969911e1c01826
Guoqi Sun <sun412926@gmail.com>
2024-12-22 16:51:09 +0800

    feat: add astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -12,5 +14,5 @@ export default defineConfig({
       plugins: () => [],
     },
   },
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), tailwind(), mdx(), sitemap()],
 })
